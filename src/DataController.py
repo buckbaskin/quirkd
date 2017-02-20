148674aca9c31d4cf8f2c076030e8a3d7ed84aeb
Buck Baskin <buckbaskin@google.com>
2017-02-20 18:33:16 -0500

    Update interfaces for DataController

--- src/DataController.py
+++ src/DataController.py
@@ -10,8 +10,17 @@ CONTROLLER = DataController()
 def scan_callback(scan_msg):
     CONTROLLER.sensor_update_scan(scan_msg)
 
-map_info_base = rospy.ServiceProxy('/quirkd/map/base/info', MapInfo)
-# map_update_base = rospy.ServiceProxy('/quirkd/map/base/update', MapUpdate)
+MAP_INFO_BASE = rospy.ServiceProxy('/quirkd/map/base/info', MapInfo)
+def map_info_base(*args, **vargs):
+    return MAP_INFO_BASE(*args, **vargs)
+
+MAP_INFO_SCAN = rospy.ServiceProxy('/quirkd/map/scan/info', MapUpdate)
+def map_info_scan(*args, **vargs):
+    return MAP_INFO_SCAN(*args, **vargs)
+
+MAP_UPDATE_SCAN = rospy.ServiceProxy('/quirkd/map/scan/update', MapUpdate)
+def map_update_scan(*args, **vargs):
+    return MAP_UPDATE_SCAN(*args, **vargs)
 
 ALERT_PUB = rospy.Publisher('/quirkd/alert/notification', Alert)
 
@@ -21,7 +30,7 @@ def publish_alert(alert):
 def handle_user_action(request):
     return UserActionResponse(CONTROLLER.user_action(request))
 
-USER_ACTION_SERVICE = ropsy.Service('/quirkd/alert/response', UserAction, handle_user_action)
+USER_ACTION_SERVICE = rospy.Service('/quirkd/alert/response', UserAction, handle_user_action)
 
 if __name__ == '__main__':
     rospy.init_node('DataController')
--- src/quirkd_lib/controller.py
+++ src/quirkd_lib/controller.py
@@ -1,2 +1,14 @@
+
 class DataController(object):
-    pass
+    def __init__(self):
+        pass
+
+    def init(self, map_info, alert_pub):
+        self.map_info = map_info
+        self.alert_pub = alert_pub
+
+    def sensor_update_scan(self, scan_msg):
+        pass
+
+    def user_action(self, request):
+        pass
