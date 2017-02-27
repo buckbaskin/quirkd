ca3ae1e7e1c568684a78b7cef6be593329e0138c
Buck Baskin <buckbaskin@google.com>
2017-02-27 13:38:06 -0500

    Get the DataController running

--- src/DataController.py
+++ src/DataController.py
@@ -22,7 +22,7 @@ MAP_UPDATE_SCAN = rospy.ServiceProxy('/quirkd/map/scan/update', MapUpdate)
 def map_update_scan(*args, **vargs):
     return MAP_UPDATE_SCAN(*args, **vargs)
 
-ALERT_PUB = rospy.Publisher('/quirkd/alert/notification', Alert)
+ALERT_PUB = rospy.Publisher('/quirkd/alert/notification', Alert, queue_size=10)
 
 def publish_alert(alert):
     ALERT_PUB.publish(alert)
@@ -34,4 +34,8 @@ USER_ACTION_SERVICE = rospy.Service('/quirkd/alert/response', UserAction, handle
 
 if __name__ == '__main__':
     rospy.init_node('DataController')
+    rospy.loginfo('rospy.init_node(\'DataController\')')
     CONTROLLER.init(map_info=map_info_base, alert_pub=publish_alert)
+    rospy.loginfo('CONTROLLER.init(...)')
+    rospy.loginfo('rospy.spin()')
+    rospy.spin()
