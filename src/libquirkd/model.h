8b2e5aa77cf3c3bbecb25e368a8d9646b4b4953b
Buck Baskin <buckbaskin@google.com>
2017-02-28 12:28:32 -0500

    Get costmap2d compiling

--- src/libquirkd/model.h
+++ src/libquirkd/model.h
@@ -1,10 +1,31 @@
 #ifndef __MAPHANDLER_H_INCLUDED__
 #define __MAPHANDLER_H_INCLUDED__
+#include <ros/ros.h>
+#include <costmap_2d/layer.h>
+#include <costmap_2d/layered_costmap.h>
+#include <costmap_2d/GenericPluginConfig.h>
+#include <dynamic_reconfigure/server.h>
 
-class MapHandler {
-    public:
-        void foo();
-        int bar;
-};
+namespace map_handler {
+
+    class SimpleLayer {
+        public:
+            SimpleLayer();
+            virtual void onInitialize();
+            virtual void updateBounds(double robot_x, double robot_y, double robot_yaw, double min_x, double min_y, double max_x, double max_y);
+            virtual void updateCosts(costmap_2d::Costmap2D& master_grid, int min_i, int min_j, int max_i, int max_j);
+        private:
+            void reconfigureCB(costmap_2d::GenericPluginConfig &config, uint32_t level);
+            double mark_x_, mark_y;
+            dynamic_reconfigure::Server<costmap_2d::GenericPluginConfig> *dsrv_;
+    };
+
+    class MapHandler {
+        public:
+            void foo();
+            int bar;
+    };
+
+}
 
 #endif // __MAPHANDLER_H_INCLUDED__
--- src/map_model.cpp
+++ src/map_model.cpp
@@ -13,7 +13,7 @@ int main(int argc, char**argv) {
     ros::init(argc, argv, "map_model_cpp");
 
     ros::NodeHandle n;
-    MapHandler mh;
+    map_handler::MapHandler mh;
 
     ros::Publisher pubber = n.advertise<quirkd::Alert>("/quirkd", 10);
 
