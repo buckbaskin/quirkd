9fb47fb5d67b12e9f18d4cac10249748bf85afb5
Buck Baskin <buckbaskin@google.com>
2017-03-03 01:05:46 -0500

    DC gets callback, updates, makes the service calls

--- src/DataController.cpp
+++ src/DataController.cpp
@@ -1,20 +1,29 @@
 #include <ros/ros.h>
-#include <quirkd/Alert.h>
+
 #include <nav_msgs/GetMap.h>
+#include <quirkd/Alert.h>
 #include <sensor_msgs/LaserScan.h>
+#include <tf/transform_listener.h>
 
 class DataController {
     public:
         DataController() {
             alert_pub_ = n_.advertise<quirkd::Alert>("/quirkd/alert/notification", 1);
             laser_sub_ = n_.subscribe("/base_scan", 1, &DataController::laserScanCB, this);
+            // ros::service::waitForService("static_map");
             static_map_client_ = n_.serviceClient<nav_msgs::GetMap>("static_map");
+            // ros::service::waitForService("dynamic_map");
             dynamic_map_client_ = n_.serviceClient<nav_msgs::GetMap>("dynamic_map");
         }
         void laserScanCB(const sensor_msgs::LaserScan msg) {
             ROS_INFO("Laser Scan Callback");
             updated = true;
             last_data = msg;
+            try {
+                tf_.lookupTransform("/map", "/base_laser_link", ros::Time(0), last_tf);
+            } catch (tf::TransformException &ex) {
+                ROS_WARN("tf fetch failed. %s", ex.what());
+            }
         }
         void update() {
             ROS_INFO("Update Data Processor");
@@ -37,12 +46,14 @@ class DataController {
         }
         bool updated;
         sensor_msgs::LaserScan last_data;
+        tf::StampedTransform last_tf;
     private:
         ros::NodeHandle n_;
         ros::Publisher alert_pub_;
         ros::Subscriber laser_sub_;
         ros::ServiceClient dynamic_map_client_;
         ros::ServiceClient static_map_client_;
+        tf::TransformListener tf_;
 };
 
 int main(int argc, char** argv) {
