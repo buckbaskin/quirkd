3605a74746c512aff25e3e3880c9866b1e771ad3
Buck Baskin <buckbaskin@google.com>
2017-03-03 02:40:15 -0500

    Update data controller to identify the window for Alerts

--- src/DataController.cpp
+++ src/DataController.cpp
@@ -1,9 +1,11 @@
 #include <ros/ros.h>
 
+#include <math.h>
 #include <nav_msgs/GetMap.h>
 #include <quirkd/Alert.h>
 #include <sensor_msgs/LaserScan.h>
 #include <tf/transform_listener.h>
+#include <tf/transform_datatypes.h>
 
 class DataController {
     public:
@@ -42,8 +44,14 @@ class DataController {
             updated = false;
         }
         void pub_alert_status() {
-            quirkd::Alert msg;
-            alert_pub_.publish(msg);
+            quirkd::Alert alert;
+            alert.level = 0;
+            alert.min_x = 0;
+            alert.max_x = 1;
+            alert.min_y = 0;
+            alert.max_y = 1;
+            updateAlertPerimeter(&alert, last_data, last_tf);
+            alert_pub_.publish(alert);
         }
         bool updated;
         sensor_msgs::LaserScan last_data;
@@ -55,6 +63,37 @@ class DataController {
         ros::ServiceClient dynamic_map_client_;
         ros::ServiceClient static_map_client_;
         tf::TransformListener tf_;
+        void updateAlertPerimeter(quirkd::Alert* alert, const sensor_msgs::LaserScan scan, const tf::StampedTransform tf) {
+            double base_x = tf.getOrigin().x();
+            double base_y = tf.getOrigin().y();
+            double r, p, base_heading;
+            tf::Matrix3x3 m(tf.getRotation());
+            m.getRPY(r, p, base_heading);
+
+            double scan_min = base_heading + scan.angle_min;
+            double scan_inc = scan.angle_increment;
+            double heading = scan_min;
+
+            alert->min_x = base_x;
+            alert->max_x = base_x;
+            alert->min_y = base_y;
+            alert->max_y = base_y;
+
+            double live_x, live_y;
+
+            for (int i = 0; i < scan.ranges.size(); i++) {
+                double dist = scan.ranges[i];
+                
+                live_x = dist * sin(heading);
+                live_y = dist * cos(heading);
+                alert->min_x = std::min(live_x, double(alert->min_x));
+                alert->max_x = std::max(live_x, double(alert->max_x));
+                alert->min_y = std::min(live_y, double(alert->min_y));
+                alert->max_y = std::max(live_y, double(alert->max_y));
+
+                heading += scan_inc;
+            }
+        }
 };
 
 int main(int argc, char** argv) {
