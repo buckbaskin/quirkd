a595b29f5657d0fb4d575a3e359e033183555e33
Buck Baskin <buckbaskin@google.com>
2017-03-30 00:33:21 -0400

    Tune up the model launch files

--- scripts/launch_model.sh
+++ scripts/launch_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 roslaunch quirkd quirkd_model.launch &
-rosrun --debug gmapping slam_gmapping scan:=base_scan map:=gmap &&
+rosrun --debug gmapping slam_gmapping scan:=base_scan map:=/dynamic_map &&
 wait %1 %2
 echo Done
