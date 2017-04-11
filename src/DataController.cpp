c0f40b2c920cf6793ab08d78054b8657014c2d27
Buck Baskin <buckbaskin@google.com>
2017-04-10 22:33:11 -0400

    Update DC to use new lines

--- src/DataController.cpp
+++ src/DataController.cpp
@@ -30,7 +30,7 @@ class DataController {
             dynamic_map_client_ = n_.serviceClient<nav_msgs::GetMap>("dynamic_map");
             static_image_pub_ = it_.advertise("/quirkd/test/static_image", 1);
             dynamic_image_pub_ = it_.advertise("/quirkd/test/dynamic_image", 1);
-            heatmap_pub_ = it_.advertise("/quirkd/test/heatmap", 1);
+            visualization_pub_ = it_.advertise("/quirkd/test/visualization", 1);
         }
         void laserScanCB(const sensor_msgs::LaserScan msg) {
             ROS_INFO("Laser Scan Callback");
@@ -82,36 +82,8 @@ class DataController {
             // two images (static (base) image and dynamic image)
             // perimeter encoded as part of the alert and images aligned to alert perimeter
 
-            /* Compute difference
-             * Steps:
-             * - manhattan distance
-             */
-
-            // Manhattan Distance
-            cv::Mat blurryStatic;
-            cv::Mat blurryDynamic;
-            cv::blur(static_image->image, blurryStatic, cv::Size(5, 5));
-            cv::blur(dynamic_image->image, blurryDynamic, cv::Size(5, 5));
-            cv::Mat absdiff;
-            cv::absdiff(blurryStatic, blurryDynamic, absdiff);
-            cv::convertScaleAbs(absdiff, absdiff, 2.55, 0.0);
-            ROS_INFO("Scale by 2.55");
-
-            // use absdiff as a mask to visualize difference with "heatmap" (bgr)
-            cv::Mat redBase(static_image->image.rows, static_image->image.cols, CV_8UC3, cv::Scalar(0, 0, 255));
-            cv::Mat byChannel[3];
-            cv::split(redBase, byChannel);
-            byChannel[2] = absdiff;
-            cv::merge(byChannel, 3, redBase);
-
-            cv_bridge::CvImage heatmap;
-            heatmap.header = static_image->header;
-            heatmap.encoding = sensor_msgs::image_encodings::BGR8;
-            heatmap.image = redBase;
-            ROS_INFO("publish heatmap part 2");
-            heatmap_pub_.publish(heatmap.toImageMsg());
-            
-            alert.level = cv::sum(absdiff)[0];
+            // use a single method that captures preprocessing and quantification
+            alert.level = this->measureDifference(*static_image, *dynamic_image);
 
             // TODO publish difference
             ROS_INFO("The two images have a difference of %d", alert.level);
@@ -127,7 +99,7 @@ class DataController {
         image_transport::ImageTransport it_;
         image_transport::Publisher static_image_pub_;
         image_transport::Publisher dynamic_image_pub_;
-        image_transport::Publisher heatmap_pub_;
+        image_transport::Publisher visualization_pub_;
         ros::Publisher alert_pub_;
         ros::Subscriber laser_sub_;
         ros::ServiceClient dynamic_map_client_;
@@ -302,6 +274,117 @@ class DataController {
 
             cv_ptr->image = base;
             return cv_ptr;
+        }
+        double distance_measure(int start_x, int start_y, int end_x, int end_y) {
+            return pow(start_x - end_x, 2) + pow(start_y - end_y, 2);
+        }
+        void preprocessImages(cv::Mat* static_image, cv::Mat* dynamic_image) {
+        }
+        int quantifyDifference(cv::Mat* static_processed, cv::Mat* dynamic_processed) {
+            /* Compute difference
+             * Steps:
+             * - identify edges
+             * - filter out matching edges
+             * - look at the size of edges and sum edge differences
+             */
+
+            cv::Mat static_edges, dynamic_edges, static_cdst, dynamic_cdst, unmatched_cdst;
+            cv::Canny(*static_processed, static_edges, 50, 200, 3); // TODO check these parameters
+            cv::Canny(*dynamic_processed, dynamic_edges, 50, 200, 3);
+
+            cv::cvtColor(static_edges, static_cdst, CV_GRAY2BGR);
+            cv::cvtColor(dynamic_edges, dynamic_cdst, CV_GRAY2BGR);
+            cv::cvtColor(dynamic_edges, unmatched_cdst, CV_GRAY2BGR);
+            
+            // Hough Parameters
+            std::vector<cv::Vec4i> static_lines, dynamic_lines;
+            const int radius_resolution = 1;
+            const double theta_resolution = CV_PI/180 / 4;
+            const int min_line_length = 8;
+            const int threshold = min_line_length; // number of interesections to make a line
+            const int max_line_gap = min_line_length - 2; // largest gap (in pixels) considered on the same line
+            
+            cv::HoughLinesP(static_edges, static_lines, radius_resolution, theta_resolution, threshold, min_line_length, max_line_gap);
+            for( size_t i = 0; i < static_lines.size(); i++ ) {
+                cv::Vec4i l = static_lines[i];
+                cv::line( static_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+            }
+            cv::HoughLinesP(dynamic_edges, dynamic_lines, radius_resolution, theta_resolution, threshold, min_line_length, max_line_gap);
+            for( size_t i = 0; i < dynamic_lines.size(); i++ ) {
+                cv::Vec4i l = dynamic_lines[i];
+                cv::line( dynamic_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+            }
+            double unmatched_quantifier = 0.0;
+
+            // find unmatched dynamic lines (red)
+            std::vector<cv::Vec4i> unmatched_lines = this->filterEdgesByMatching(static_lines, dynamic_lines);
+            for( size_t i = 0; i < unmatched_lines.size(); i++ ) {
+                cv::Vec4i l = unmatched_lines[i];
+                cv::line( unmatched_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+                unmatched_quantifier += this->distance_measure(l[0], l[1], l[2], l[3]);
+            }
+            // find unmatched static lines (red)
+            unmatched_lines = this->filterEdgesByMatching(dynamic_lines, static_lines);
+            for( size_t i = 0; i < unmatched_lines.size(); i++ ) {
+                cv::Vec4i l = unmatched_lines[i];
+                cv::line( unmatched_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,255,0), 2, CV_AA);
+                unmatched_quantifier += this->distance_measure(l[0], l[1], l[2], l[3]);
+            }
+            
+            int alert_level = (int) (unmatched_quantifier);
+
+            cv_bridge::CvImage anomaly_visualization;
+            anomaly_visualization.encoding = sensor_msgs::image_encodings::BGR8;
+            anomaly_visualization.image = unmatched_cdst;
+            ROS_INFO("publish visualization");
+            visualization_pub_.publish(anomaly_visualization.toImageMsg());
+            
+            // return sum of the absdiff
+            return alert_level;
+        }
+        std::vector<cv::Vec4i> filterEdgesByMatching(std::vector<cv::Vec4i> original, std::vector<cv::Vec4i> new_edges) {
+            std::vector<cv::Vec4i> unmatched_set;
+            const double limit = 1000;
+            for( size_t i = 0; i < new_edges.size(); i++ ) { // for each edge in the new edges
+                cv::Vec4i seeker = new_edges[i];
+                bool matched = false;
+                double min_best = 10000000;
+                for( size_t i = 0; i < original.size(); i++ ) { // check each of the original edges
+                    cv::Vec4i candidate = original[i];
+                    // for the beginning and end of the seeker line, check the distance to the candidate endpoints
+                    // take the orientation with the best overall score
+                    // if it beats (smaller than) the existing best match, update the params
+
+                    // front to front
+                    double forward_front = this->distance_measure(seeker[0], seeker[1], candidate[0], candidate[1]);
+                    double forward_back = this->distance_measure(seeker[2], seeker[3], candidate[2], candidate[3]);
+                    if (forward_front < limit/2 && forward_back < limit/2 && forward_back + forward_front < limit/2) {
+                        matched = true;
+                        break;
+                    }
+                    min_best = std::min(min_best, forward_front + forward_back);
+                    double reverse_front = this->distance_measure(seeker[0], seeker[1], candidate[2], candidate[3]);
+                    double reverse_back = this->distance_measure(seeker[2], seeker[3], candidate[0], candidate[1]);
+                    if (reverse_front < limit/2 && reverse_back < limit/2 && reverse_back + reverse_front < limit/2) {
+                        matched = true;
+                        break;
+                    }
+                    min_best = std::min(min_best, reverse_front + reverse_back);
+                }
+                // if there isn't a good match, add it to the unmatched set
+                if (!matched) {
+                    ROS_INFO("Best min %f", min_best);
+                    unmatched_set.push_back(seeker);
+                } else {
+                    // I found a good match!
+                }
+            }
+            return unmatched_set;
+        }
+        int measureDifference(cv_bridge::CvImage static_image, cv_bridge::CvImage dynamic_image) {
+            this->preprocessImages(&(static_image.image), &(dynamic_image.image));
+            int result = this->quantifyDifference(&(static_image.image), &(dynamic_image.image));
+            return result;
         }
 };
 
