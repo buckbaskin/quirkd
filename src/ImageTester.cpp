9a28f73da81d8782b9fa06119ffe5f8841652c68
Buck Baskin <buckbaskin@google.com>
2017-04-10 20:36:51 -0400

    Edge detection seems to work nicely. Now I need to pair edges

--- src/ImageTester.cpp
+++ src/ImageTester.cpp
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <sstream>
 
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
@@ -35,19 +36,42 @@ class ImageTester {
         int quantifyDifference(cv::Mat* static_processed, cv::Mat* dynamic_processed) {
             // src = static_processed
 
-            cv::imshow("source", *static_processed);
-            cv::Mat dst, cdst;
-            cv::Canny(*static_processed, dst, 50, 200, 3); // TODO check these parameters
-            cv::imshow("dst after Canny", dst);
-            cv::cvtColor(dst, cdst, CV_GRAY2BGR);
-            std::vector<cv::Vec4i> lines;
-            cv::HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10); // TODO check these parameters
-            for( size_t i = 0; i < lines.size(); i++ ) {
-                cv::Vec4i l = lines[i];
-                cv::line( cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+            ROS_INFO("src size %d x %d", (*static_processed).cols, (*static_processed).rows);
+            // cv::imshow("source", *static_processed);
+            cv::Mat static_edges, dynamic_edges, static_cdst, dynamic_cdst;
+            cv::Canny(*static_processed, static_edges, 50, 200, 3); // TODO check these parameters
+            cv::Canny(*dynamic_processed, dynamic_edges, 50, 200, 3);
+
+            cv::imshow("static_edges after Canny", static_edges);
+            cv::imshow("dynamic_edges after Canny", dynamic_edges);
+
+            cv::cvtColor(static_edges, static_cdst, CV_GRAY2BGR);
+            cv::cvtColor(dynamic_edges, dynamic_cdst, CV_GRAY2BGR);
+            
+            // Hough Parameters
+            std::vector<cv::Vec4i> static_lines, dynamic_lines;
+            const int radius_resolution = 1;
+            const double theta_resolution = CV_PI/180 / 4;
+            const int min_line_length = 8;
+            const int threshold = min_line_length; // number of interesections to make a line
+            const int max_line_gap = min_line_length - 2; // largest gap (in pixels) considered on the same line
+            std::string stitle = "detected lines ";
+
+            cv::HoughLinesP(static_edges, static_lines, radius_resolution, theta_resolution, threshold, min_line_length, max_line_gap);
+            for( size_t i = 0; i < static_lines.size(); i++ ) {
+                cv::Vec4i l = static_lines[i];
+                cv::line( static_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
             }
-            cv::imshow("detected lines", cdst);
-            cv::waitKey(0);
+            cv::HoughLinesP(dynamic_edges, dynamic_lines, radius_resolution, theta_resolution, threshold, min_line_length, max_line_gap);
+            for( size_t i = 0; i < dynamic_lines.size(); i++ ) {
+                cv::Vec4i l = dynamic_lines[i];
+                cv::line( dynamic_cdst, cv::Point(l[0], l[1]), cv::Point(l[2], l[3]), cv::Scalar(0,0,255), 3, CV_AA);
+            }
+            std::ostringstream title;
+            title << stitle << min_line_length;
+            cv::imshow("static detected", static_cdst);
+            cv::imshow("dynamic detected", dynamic_cdst);
+            
 
             // OLD VERSION
             cv::Mat absdiff;
@@ -66,23 +90,17 @@ class ImageTester {
             heatmap.image = redBase;
             ROS_INFO("publish heatmap");
             heatmap_pub_.publish(heatmap.toImageMsg());
-            cv::imshow("Absdiff", absdiff);
+            // cv::imshow("Absdiff", absdiff);
 
             // return sum of the absdiff
             return cv::sum(absdiff)[0];
         }
         int measureDifference(cv_bridge::CvImage static_image, cv_bridge::CvImage dynamic_image) {
-            // two images (static (base) image and dynamic image)
-            // perimeter encoded as part of the alert and images aligned to alert perimeter
-
-            /* Compute difference
-             * Steps:
-             * - manhattan distance
-             */
-
-            // Manhattan Distance
+            ROS_INFO("static map original size %d x %d", static_image.image.cols, static_image.image.rows);
             this->preprocessImages(&(static_image.image), &(dynamic_image.image));
+            ROS_INFO("static map size %d x %d", static_image.image.cols, static_image.image.rows);
             cv::imshow("Static Map", static_image.image);
+            ROS_INFO("dynamic map size %d x %d", dynamic_image.image.cols, dynamic_image.image.rows);
             cv::imshow("Dynamic Map", dynamic_image.image);
             int result = this->quantifyDifference(&(static_image.image), &(dynamic_image.image));
             cv::waitKey(0);
@@ -102,8 +120,16 @@ int main(int argc, char** argv) {
     cv_bridge::CvImage image1;
     cv_bridge::CvImage image2;
     image1.image = cv::imread("images/filterInProgress/static001.png", cv::IMREAD_GRAYSCALE);
+    ROS_INFO("imread1 size %d x %d", image1.image.cols, image1.image.rows);
     image2.image = cv::imread("images/filterInProgress/dynamic001.png", cv::IMREAD_GRAYSCALE);
+    
+    if (!(image1.image.cols > 0 && image1.image.rows > 0 && image2.image.cols > 0 && image2.image.rows > 0)) {
+        ROS_ERROR("Did not properly load images");
+        return 1;
+    }
+
     int result = it.measureDifference(image1, image2);
+
     ROS_INFO("ImageTester Exited. Image diff of %d", result);
     return 0;
 }
