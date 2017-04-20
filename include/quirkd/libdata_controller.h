4f587fa78a0d17391848569de1b5c7384d97b4a7
Buck Baskin <buckbaskin@google.com>
2017-04-20 00:28:08 -0400

    Auto format the header files

--- include/quirkd/libdata_controller.h
+++ include/quirkd/libdata_controller.h
@@ -3,16 +3,16 @@
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the 
+ * deal in the Software without restriction, including without limitation the
  * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is 
+ * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in 
+ *
+ * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
@@ -49,23 +49,23 @@ public:
   ~DataController();
   void laserScanCB(const sensor_msgs::LaserScan msg);
   void update();
-  
+
 private:
   ros::NodeHandle n_;
   ros::Publisher alert_pub_;
   ros::Subscriber laser_sub_;
   ros::ServiceClient dynamic_map_client_;
   ros::ServiceClient static_map_client_;
-  
+
   image_transport::ImageTransport it_;
   image_transport::Publisher static_image_pub_;
   image_transport::Publisher dynamic_image_pub_;
   image_transport::Publisher visualization_pub_;
-  
+
   sensor_msgs::LaserScan last_data;
   tf::StampedTransform last_tf;
   tf::TransformListener tf_;
-  
+
   void updateAlertPerimeter(quirkd::Alert* alert, const sensor_msgs::LaserScan scan, const tf::StampedTransform tf);
   cv_bridge::CvImagePtr gridToCroppedCvImage(nav_msgs::OccupancyGrid* grid, quirkd::Alert* alert);
   double distance_measure(int start_x, int start_y, int end_x, int end_y);
@@ -73,8 +73,9 @@ private:
   std::vector<quirkd::Alert> quantifyDifference(cv::Mat* static_processed, cv::Mat* dynamic_processed);
   std::vector<cv::Vec4i> filterEdgesByMatching(std::vector<cv::Vec4i> original, std::vector<cv::Vec4i> new_edges);
   std::vector<quirkd::Alert> measureDifference(cv_bridge::CvImage static_image, cv_bridge::CvImage dynamic_image);
-  std::vector<quirkd::Alert> minimizeAlerts(std::vector<cv::Vec4i> unmatched_static, std::vector<cv::Vec4i> unmatched_dynamic);
+  std::vector<quirkd::Alert> minimizeAlerts(std::vector<cv::Vec4i> unmatched_static,
+                                            std::vector<cv::Vec4i> unmatched_dynamic);
 };
 
-} // namespace quirkd
-#endif // QUIRKD_DATA_CONTROLLER_H
+}  // namespace quirkd
+#endif  // QUIRKD_DATA_CONTROLLER_H
