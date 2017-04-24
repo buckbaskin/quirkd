43629325e9fb03d0d32d780fe9168ffa8457bca3
Buck Baskin <buckbaskin@google.com>
2017-04-24 13:15:15 -0400

    Various fixes, math and otherwise. It's working

--- src/thermal_laser_matcher.py
+++ src/thermal_laser_matcher.py
@@ -16,18 +16,24 @@ angle
  +---------- column
 
 column | angle (deg) | angle (rad)
-   0   |     -30     | 
-  160  |     -15     |
+   0   |      30     | 
+  160  |      15     |
   320  |       0     |
-  480  |      15     |
+  480  |     -15     |
   640  |      30     | 
+
+            0 deg
+              |
+              |
+              |
++90 deg -----[ ]----- -90 deg
 '''
 __conversion_factor = math.pi / 180
-ANGLE_DEG_AT_COLUMN_160 = -15
+ANGLE_DEG_AT_COLUMN_160 = 15
 ANGLE_RAD_AT_COLUMN_160 = ANGLE_DEG_AT_COLUMN_160 * __conversion_factor
 ANGLE_DEG_AT_COLUMN_320 = 0
 ANGLE_RAD_AT_COLUMN_320 = ANGLE_DEG_AT_COLUMN_320 * __conversion_factor
-ANGLE_DEG_AT_COLUMN_480 = 15
+ANGLE_DEG_AT_COLUMN_480 = -15
 ANGLE_RAD_AT_COLUMN_480 = ANGLE_DEG_AT_COLUMN_480 * __conversion_factor
 '''
 y = ax + b
@@ -48,6 +54,7 @@ MinDistPublisher = None
 
 def publish_distance(dist, angle):
     # publishes r, theta, z = 0
+    rospy.loginfo('Match thermal dist. (%.2f, %.2f, 0.0)' % (dist, angle,))
     v = Vector3()
     v.x = dist
     v.y = angle
@@ -55,16 +62,22 @@ def publish_distance(dist, angle):
         ThermalMatchPublisher.publish(v)
 
 def publish_minimum_angle(dist, angle):
-    # publish the distance, angle, z of the minimum laser scan distance
+    '''
+    Publish the distance, angle, z of the minimum laser scan distance
+    '''
+    # rospy.loginfo('Match minimum scan angle. (%.2f, %.2f, 0.0)' % (dist, angle / math.pi * 180,))
     v = Vector3()
     v.x = dist
     v.y = angle
     if MinDistPublisher is not None:
-        MinDistPublisher.publish(msg)
+        MinDistPublisher.publish(v)
 
 def laser_callback(msg):
     # saves scan for matching to centroid
     # also publishes the minimum distance to a point on the laser scan
+    global last_scan
+    if last_scan is None:
+        rospy.loginfo('Saving first laser scan')
     last_scan = msg
 
     angle_min = msg.angle_min
@@ -81,10 +94,9 @@ def laser_callback(msg):
         else:
             dist_accum += value
             dist_accum -= msg.ranges[index - average_this_many]
-        if dist_accum / average_this_many < min_observed_dist:
-            min_observed_dist = dist_accum / average_this_many
-            min_observed_angle = angle_min + ((index - 1) * angle_inc)
-    rospy.loginfo('Match minimum scan angle.')
+            if dist_accum / average_this_many < min_observed_dist:
+                min_observed_dist = dist_accum / average_this_many
+                min_observed_angle = angle_min + ((index - 1) * angle_inc)
     publish_minimum_angle(min_observed_dist, min_observed_angle)
 
 def centroid_callback(msg):
@@ -94,14 +106,18 @@ def centroid_callback(msg):
     if last_scan is not None:
         # TODO look at last scan and match nearest distance here
         pass
-    rospy.loginfo('Match centroid to scan distance.')
+        rospy.loginfo('Match centroid to scan distance.')
+    else:
+        rospy.loginfo('Cannot match centroid. Laser Scan not yet found.')
     publish_distance(distance, centroid_angle_radians)
 
 def listener():
     rospy.init_node('thermal_laser_matcher')
-    rospy.Subscriber("/laser_scan", LaserScan, laser_callback)
+    rospy.Subscriber("/base_scan", LaserScan, laser_callback)
     rospy.Subscriber("/centroid", Float64, centroid_callback)
+    global ThermalMatchPublisher
     ThermalMatchPublisher = rospy.Publisher("/thermal_match", Vector3, queue_size=10)
+    global MinDistPublisher
     MinDistPublisher = rospy.Publisher("/min_dist_to_scan", Vector3, queue_size=10)
 
     rospy.loginfo('Begin Thermal Laser Matching.')
